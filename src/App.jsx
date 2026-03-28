62cc48630abb1f5c4cb15cace8c77c88a2e6c367
MD. Sabbir Hossain <sabbirhossain8721@gmail.com>
2026-03-28 22:02:12 +0600

    Axios JS chart data processing and bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import DaisyNav from './components/DaisyNav/DaisyNav';
 import NavBar from './components/NavBar/NavBar';
 import PricingOptions from './components/PricingOptions/PricingOptions';
 import ResultsCharts from './components/ResultsCharts/ResultsCharts';
+import axios from 'axios';
+import MarksChart from './components/MarksChart/MarksChart';
 
 
 const pricingPromise = fetch('pricingPlans.json').then(res => res.json());
+const marksPromise = axios.get('marksData.json');
 
 function App() {
 
@@ -21,8 +24,11 @@ function App() {
         <main>
            <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
               <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
-
               <ResultsCharts></ResultsCharts>
+           </Suspense>
+
+           <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+              <MarksChart marksPromise={marksPromise}></MarksChart>
            </Suspense>
         </main>
 
